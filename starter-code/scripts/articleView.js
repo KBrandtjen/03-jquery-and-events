b59d01aec942009503ce2b2059fd5ccf071821e0
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-08-17 12:12:19 -0700

    changes made during class

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -1,7 +1,16 @@
 // IN-CLASS TODO: Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
 
-articleView.populateFilters = function() {
+var articleView = {};
 
+
+
+articleView.populateFilters = function() {
+  $('article').not('.template').each(function() {
+    var authorName, category, optionTag;
+    authorName = $(this).find('address a').text();
+    optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+    $('#author-filter').append(optionTag);
+  });
 };
 
 articleView.handleAuthorFilter = function() {
